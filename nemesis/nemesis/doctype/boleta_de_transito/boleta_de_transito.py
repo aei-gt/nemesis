be3029ab67413741f0fc1baa4ea495a9dd293606
raza9100 <ahmedrazapk>
2024-09-15 21:30:46 +0500

    bug fix

--- nemesis/nemesis/doctype/boleta_de_transito/boleta_de_transito.py
+++ nemesis/nemesis/doctype/boleta_de_transito/boleta_de_transito.py
@@ -4,15 +4,12 @@
 import frappe
 from frappe.model.document import Document
 from frappe.utils import get_link_to_form
-
-
 class BoletadeTransito(Document):
-	def validate(self):
-		boletas_doc_list= frappe.get_all(self.doctype, {"boleta_id": self.boleta_id, "docstatus": ["<", 2] })
-		if len(boletas_doc_list) > 0:
-			frappe.throw(f"Boleta No already used in {get_link_to_form(self.doctype, boletas_doc_list[0].name)}")
+    def validate(self):
+        boletas_doc_list = frappe.get_all(self.doctype,filters={"boleta_id": self.boleta_id,"docstatus": ["<", 2],"name": ["!=", self.name]})
+        if len(boletas_doc_list) > 0:
+            frappe.throw(f"Boleta No already used in {get_link_to_form(self.doctype, boletas_doc_list[0].name)}")
 	
-# @frappe.validate_and_sanitize_search_inputs
 @frappe.whitelist()
 def boletas_doc_query(doctype, txt, searchfield, start, page_len, filters):
 	boletas_doc_list= frappe.get_all("Boleta de Transito", {}, "boleta_id")
